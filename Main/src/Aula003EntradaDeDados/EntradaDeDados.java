73e95e8017be0d1ec6e6021f011957c45e5924e8
nayarakarine.araujo@gmail.com <nayarakarine.araujo@gmail.com>
2024-08-03 18:37:04 -0300

    Aula de Entrada de Dados

--- Main/src/Aula003EntradaDeDados/EntradaDeDados.java
+++ Main/src/Aula003EntradaDeDados/EntradaDeDados.java
@@ -1,4 +1,25 @@
 package Aula003EntradaDeDados;
 
+import java.util.Scanner;
+
 public class EntradaDeDados {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Olá ");
+
+        //String x;
+        //x = sc.next(); //Entrada de texto
+
+//        double x;
+//        x = sc.nextDouble(); //Entrada de número flutuante
+
+        char x;
+        x = sc.next().charAt(0);
+        System.out.println("Você digitou: " + x);
+
+        sc.close();
+
+    }
 }

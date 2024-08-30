96ed724b4ecafcfbf9677c1f3258279e2bead316
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-08-30 10:07:32 -0300

    Estrutura de repetição

--- Main/src/EstruturaRepetitiva/EstruturaRepetitivaWhile.java
+++ Main/src/EstruturaRepetitiva/EstruturaRepetitivaWhile.java
@@ -1,4 +1,35 @@
 package EstruturaRepetitiva;
 
+import java.awt.*;
+import java.util.Scanner;
+
 public class EstruturaRepetitivaWhile {
+    public static void main(String[] args) {
+
+        //ESTRUTURA REPETITIVA WHILE(ENQUANTO)
+/*
+É uma etrutura de controle que repete um bloco de comandos enquanto
+uma condição for verdadeira.
+Quando usar: quando não se sabe previamente a quantidade de repetições
+que será realizada.
+
+PROBLEMA EXEMPLO
+Fazer um programa que lê números inteiros até que um zero
+seja lido. Ao final mostra a soma dos números lidos.
+
+*/
+
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+
+        int soma = 0;
+        while( x != 0) {
+            soma = soma + x;
+            //ou soma += x
+            x = scanner.nextInt();
+        }
+        System.out.println("A soma dos números digitados é: " + soma);
+        scanner.close();
+    }
 }

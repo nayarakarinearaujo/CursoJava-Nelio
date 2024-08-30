892ff67da76056c661b29a2a670077e3dc9235ed
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-08-30 12:46:00 -0300

    Estrutura de repetição

--- Main/src/EstruturaRepetitiva/EstruturaRepetitivaFor.java
+++ Main/src/EstruturaRepetitiva/EstruturaRepetitivaFor.java
@@ -1,4 +1,14 @@
 package EstruturaRepetitiva;
 
 public class EstruturaRepetitivaFor {
+    public static void main(String[] args) {
+
+        //ESTRUTURA REPETITIVA FOR(PARA)
+/*
+É uma estrutura de controle que repete um bloco de comandos para um certo intervalo
+de valores.
+Quando usar: quando se sabe previamente a quantidade de repetições,
+ou o intervalo de valores.
+*/
+    }
 }
